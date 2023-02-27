b0ddfbfb2532dc2c467ebbc2075d6dee4fec1880
G00295382 <123401056+G00295382@users.noreply.github.com>
2023-02-27 12:34:45 -0500

    bulk work

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -4,12 +4,19 @@
  * @author David J. Barnes and Michael Kölling.
  * @version    2016.02.29
  */
+import java.util.Scanner;
+
 public class LogAnalyzer
 {
     // Where to calculate the hourly access counts.
     private int[] hourCounts;
+    private int[] dayCounts;
+    private int[] monthCounts;
+    private int[] yearCounts;
     // Use a LogfileReader to access the data.
     private LogfileReader reader;
+    
+    Scanner keyboard = new Scanner(System.in);
 
     /**
      * Create an object to analyze hourly web accesses.
@@ -19,20 +26,105 @@ public class LogAnalyzer
         // Create the array object to hold the hourly
         // access counts.
         hourCounts = new int[24];
+        dayCounts = new int[29];
+        monthCounts = new int[13];
+        yearCounts = new int[5];
         // Create the reader to obtain the data.
         reader = new LogfileReader("demo.log");
+        this.analyzeData();
+    }
+    
+    public LogAnalyzer(String log)
+    { 
+        // Create the array object to hold the hourly
+        // access counts.
+        hourCounts = new int[24];
+        dayCounts = new int[29];
+        monthCounts = new int[13];
+        yearCounts = new int[5];
+        // Create the reader to obtain the data.
+        reader = new LogfileReader(log);
+        this.analyzeData();
     }
 
     /**
      * Analyze the hourly access data from the log file.
      */
-    public void analyzeHourlyData()
+    public void analyzeData()
     {
+        int i = 0;
         while(reader.hasNext()) {
+            i++;
+            System.out.println("line " + i);
             LogEntry entry = reader.next();
             int hour = entry.getHour();
+            int day = entry.getDay();
+            int month = entry.getMonth();
+            System.out.println("month : " + month);
+            int year = entry.getYear();
             hourCounts[hour]++;
+            dayCounts[day]++;
+            monthCounts[month]++;
+            yearCounts[year]++;
         }
+    }
+    
+    public void numberOfAccesses()
+    {
+        int accesses = 0;
+        while(reader.hasNext()) {
+            accesses += 1;
+            LogEntry entry = reader.next();
+        }
+        System.out.println("This file has been accessed " + accesses + " times.");
+    }
+    
+    public void busiestHour()
+    {
+        int max = hourCounts[0];
+        int maxhour = 0;
+        for(int hour = 0; hour < hourCounts.length; hour++) {
+            //System.out.println("Hour : " + hour + ", Value : " + hourCounts[hour]);
+            if(hourCounts[hour] > max)
+            {
+                max = hourCounts[hour];                
+                maxhour = hour;
+                //System.out.println("Max changed");
+            }
+        }
+        System.out.println("The busiest hour is " + maxhour + " with " + max + " accesses.");
+    }
+    
+    public void busiestTwoHour()
+    {
+        int max = hourCounts[0] + hourCounts[1];
+        String maxhours = "0 and 1";
+        for(int hour = 2; hour < hourCounts.length; hour += 2) {
+            //System.out.println("Hour : " + hour + ", Value : " + hourCounts[hour]);
+            if((hourCounts[hour] + hourCounts[hour + 1]) > max)
+            {
+                max = (hourCounts[hour] + hourCounts[hour + 1]);                
+                maxhours = (hour) + " and " + (hour + 1);
+                //System.out.println("Max changed");
+            }
+        }
+        System.out.println("The busiest hours are " + maxhours + " with " + max + " accesses.");
+    }
+    
+    public void quietestHour()
+    {
+        int min = hourCounts[0];
+        int minhour = 0;
+        for(int hour = 1; hour < hourCounts.length; hour++) {
+            //System.out.println("Hour : " + hour + ", Value : " + hourCounts[hour]);
+            if(hourCounts[hour] < min)
+            {
+                min = hourCounts[hour];                
+                minhour = hour;
+                //System.out.println("Min changed");
+            }
+        }
+        System.out.println("The quietest hour is " + minhour + " with " + min + " accesses.");
     }
 
     /**
